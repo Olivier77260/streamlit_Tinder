130166c274c4863de475f19ed6d85be4ca406351
Olivier Evrard <50024014+Olivier77260@users.noreply.github.com>
2024-07-24 14:17:37 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,30 @@
+import streamlit as st
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
 import pandas as pd
-print("hello wordl !!! ")
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.write("hello wordl !!!")
+st.markdown("#### Hello wordl !!!")
+
+
+df = pd.read_csv("Speed_Dating_Data.csv")
+df['age'] = df['age'].apply(pd.to_numeric)
+# sns.boxplot(data=df, x=df['age'])
+# st.write(plt.title('age'))
+# st.write(plt.show())
+st.write(sns.boxplot(data=df, x=df['age']))
+chart_data = sns.boxplot(data=df, x=df['age'])
+
+st.area_chart(chart_data)
+
+# st.area_chart(df['age'], x="col1", y="col2", color="col3")
+
+fig, ax = plt.subplots()
+ax.hist(df['age'], bins=40)
+st.pyplot(fig)
+
+st.line_chart(df['age'])
