c3b4af5efdb15bded57f5fccb8f97bfe07e036e5
Olivier_pc_Portable <olivier.evrard77@gmail.com>
2024-07-25 19:37:58 +0200

    Version 25.07.1

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,41 +1,53 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-def login():
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+df = conn.read()
 
-def logout():
-    if st.button("Log out"):
-        st.session_state.logged_in = False
-        st.rerun()
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
 
-login_page = st.Page(login, title="Log in", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+df = conn.read("Speed_Dating_Data.csv")
 
-dashboard = st.Page(
-    "reports/page1.py", title="Dashboard", icon=":material/dashboard:", default=True
-)
-bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
-alerts = st.Page(
-    "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
-)
+# if "logged_in" not in st.session_state:
+#     st.session_state.logged_in = False
 
-search = st.Page("tools/search.py", title="Search", icon=":material/search:")
-history = st.Page("tools/history.py", title="History", icon=":material/history:")
+# def login():
+#     if st.button("Log in"):
+#         st.session_state.logged_in = True
+#         st.rerun()
 
-if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "Account": [logout_page],
-            "Reports": [dashboard, bugs, alerts],
-            "Tools": [search, history],
-        }
-    )
-else:
-    pg = st.navigation([login_page])
+# def logout():
+#     if st.button("Log out"):
+#         st.session_state.logged_in = False
+#         st.rerun()
 
-pg.run()
+# login_page = st.Page(login, title="Log in", icon=":material/login:")
+# logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+
+# dashboard = st.Page(
+#     "reports/page1.py", title="Dashboard", icon=":material/dashboard:", default=True
+# )
+# bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
+# alerts = st.Page(
+#     "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
+# )
+
+# search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+# history = st.Page("tools/history.py", title="History", icon=":material/history:")
+
+# if st.session_state.logged_in:
+#     pg = st.navigation(
+#         {
+#             "Account": [logout_page],
+#             "Reports": [dashboard, bugs, alerts],
+#             "Tools": [search, history],
+#         }
+#     )
+# else:
+#     pg = st.navigation([login_page])
+
+# pg.run()
