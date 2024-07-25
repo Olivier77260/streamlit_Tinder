b54ca86d9bf10b70a0360b735b41ff36ccef1fd8
Olivier Evrard <50024014+Olivier77260@users.noreply.github.com>
2024-07-25 14:07:12 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,41 @@
 import streamlit as st
 
-create_page = st.Page("page1.py", title="Create entry 1")
-delete_page = st.Page("page2.py", title="Delete entry")
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-pg = st.navigation([create_page, delete_page])
-st.set_page_config(page_title="Data manager")
+def login():
+    if st.button("Log in"):
+        st.session_state.logged_in = True
+        st.rerun()
+
+def logout():
+    if st.button("Log out"):
+        st.session_state.logged_in = False
+        st.rerun()
+
+login_page = st.Page(login, title="Log in", icon=":material/login:")
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+
+dashboard = st.Page(
+    "reports/page1.py", title="Dashboard", icon=":material/dashboard:", default=True
+)
+bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
+alerts = st.Page(
+    "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
+)
+
+search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+history = st.Page("tools/history.py", title="History", icon=":material/history:")
+
+if st.session_state.logged_in:
+    pg = st.navigation(
+        {
+            "Account": [logout_page],
+            "Reports": [dashboard, bugs, alerts],
+            "Tools": [search, history],
+        }
+    )
+else:
+    pg = st.navigation([login_page])
+
 pg.run()
