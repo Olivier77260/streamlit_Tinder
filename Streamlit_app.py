e44d9d79cf3bd7379c6dfc7146c6f7b04e3a694d
Olivier Evrard <50024014+Olivier77260@users.noreply.github.com>
2024-07-25 13:38:20 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
 
-create_page = st.Page("page1.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("page2.py", title="Delete entry", icon=":material/delete:")
+create_page = st.Page("page1.py", title="Create entry")
+delete_page = st.Page("page2.py", title="Delete entry")
 
 pg = st.navigation([create_page, delete_page])
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+st.set_page_config(page_title="Data manager")
 pg.run()
